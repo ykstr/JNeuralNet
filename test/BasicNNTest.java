5b0420e7fca599796b8e0628364aa4ffad936ea1
yannic <yannickuester@gmail.com>
2018-03-30 16:04:58 +0200

    added a simple test implementation inside the main method to test for exceptions or unexpected result values like NaN (which can e.g. occur if a non-input neuron is not connected to any previous neurons or none of the connected neurons have a weight != 0)

--- test/BasicNNTest.java
+++ test/BasicNNTest.java
@@ -1,9 +1,20 @@
+import de.ykstr.jneuralnet.components.BasicNetwork;
 import de.ykstr.jneuralnet.components.layers.InputLayer;
 import de.ykstr.jneuralnet.components.neurons.InputNeuron;
 import de.ykstr.jneuralnet.components.neurons.WeightedNeuron;
 import de.ykstr.jneuralnet.functions.LinearActivation;
 
+import java.util.ArrayList;
+
 public class BasicNNTest {
     public static void main(String[] args) {
+        BasicNetwork network = new BasicNetwork(15, LinearActivation.getInstance());
+        network.addInput(new InputNeuron(LinearActivation.getInstance(),Math.random()),
+                Math.random());
+        network.addInput(new InputNeuron(LinearActivation.getInstance(),Math.random()),
+                Math.random());
+        network.addInput(new InputNeuron(LinearActivation.getInstance(),Math.random()),
+                Math.random());
+        System.out.println(network.calculateOutputs());
     }
 }
