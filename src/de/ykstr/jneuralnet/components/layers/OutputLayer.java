e1642422b451ba6fda51f3e4e593f6c73a950ef0
yannic <yannickuester@gmail.com>
2018-03-30 15:10:22 +0200

    refactored OutputLayer to use the storage of AbstractLayer instead of its own ArrayList (the later was removed) added a constructor to quickly generate a layer with a certain amount of neurons added a method to fully connect a layer to a previous one

--- src/de/ykstr/jneuralnet/components/layers/OutputLayer.java
+++ src/de/ykstr/jneuralnet/components/layers/OutputLayer.java
@@ -1,11 +1,20 @@
 package de.ykstr.jneuralnet.components.layers;
 
 import de.ykstr.jneuralnet.components.neurons.WeightedNeuron;
+import de.ykstr.jneuralnet.functions.IActivationFunction;
 
-import java.util.ArrayList;
+public class OutputLayer extends AbstractLayer<WeightedNeuron>{
 
-public class OutputLayer {
-    private ArrayList<WeightedNeuron> neurons = new ArrayList<>();
+    public OutputLayer(int size, IActivationFunction f){
+        super();
+        for(int i = 0; i<size; i++){
+            addNeuron(new WeightedNeuron(f));
+        }
+    }
 
-
+    public void connectAll(AbstractLayer former){
+        for(WeightedNeuron n : neurons){
+            n.addMultipleInputs(former.getNeurons(),1.0/former.getNeurons().size());
+        }
+    }
 }
