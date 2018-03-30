7d02c4596a2ac1065f63560d2336886e18540838
yannic <yannickuester@gmail.com>
2018-03-30 15:00:19 +0200

    added simple test class to contain the main method

---
+++ test/BasicNNTest.java
@@ -0,0 +1,9 @@
+import de.ykstr.jneuralnet.components.layers.InputLayer;
+import de.ykstr.jneuralnet.components.neurons.InputNeuron;
+import de.ykstr.jneuralnet.components.neurons.WeightedNeuron;
+import de.ykstr.jneuralnet.functions.LinearActivation;
+
+public class BasicNNTest {
+    public static void main(String[] args) {
+    }
+}
