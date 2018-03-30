977066898712bc79b728408aa321c8a58d0354d9
yannic <yannickuester@gmail.com>
2018-03-30 10:42:50 +0200

    added Neuron class skeleton added ActivationFunction Interface

---
+++ src/de/ykstr/jneuralnet/components/IActivationFunction.java
@@ -0,0 +1,5 @@
+package de.ykstr.jneuralnet.components;
+
+public interface IActivationFunction {
+    public double calculate(double value);
+}
---
+++ src/de/ykstr/jneuralnet/components/Neuron.java
@@ -0,0 +1,7 @@
+package de.ykstr.jneuralnet.components;
+
+import java.util.HashMap;
+
+public class Neuron {
+    private HashMap<Neuron, Double> weightedInputs;
+}
