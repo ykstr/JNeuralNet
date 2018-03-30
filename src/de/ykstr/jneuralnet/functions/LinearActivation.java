9c367c4cf89ba2d001c3236e93c4f33001feb31f
yannic <yannickuester@gmail.com>
2018-03-30 15:10:57 +0200

    refactored LinearActivation into a Singleton behaviour

--- src/de/ykstr/jneuralnet/functions/LinearActivation.java
+++ src/de/ykstr/jneuralnet/functions/LinearActivation.java
@@ -1,6 +1,18 @@
 package de.ykstr.jneuralnet.functions;
 
 public class LinearActivation implements IActivationFunction{
+
+    private static LinearActivation instance;
+
+    private LinearActivation(){}
+
+    public static LinearActivation getInstance(){
+        if(instance == null){
+            instance = new LinearActivation();
+        }
+        return instance;
+    }
+
     @Override
     public double calculate(double value) {
         if(value<0){
