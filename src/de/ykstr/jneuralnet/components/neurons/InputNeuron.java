bcb28210ea55aa6e2c4c6de0d5955d62ac5ec78d
yannic <yannickuester@gmail.com>
2018-03-30 15:36:39 +0200

    changed InputNeuron constructor to include the input value of the neuron (which can still be changed via setValue)

--- src/de/ykstr/jneuralnet/components/neurons/InputNeuron.java
+++ src/de/ykstr/jneuralnet/components/neurons/InputNeuron.java
@@ -3,7 +3,8 @@ package de.ykstr.jneuralnet.components.neurons;
 import de.ykstr.jneuralnet.functions.IActivationFunction;
 
 public class InputNeuron extends Neuron{
-    public InputNeuron(IActivationFunction f) {
+    public InputNeuron(IActivationFunction f, double value) {
         super(f);
+        setValue(value);
     }
 }
